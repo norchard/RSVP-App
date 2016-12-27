84e724f5691542446b552aedf0b8aa1e774e0a1b
Nicole Orchard <norchard24@gmail.com>
2016-12-27 17:15:40 +0700

    Switched from SQLite3 to Postgres

--- environment.rb
+++ environment.rb
@@ -2,6 +2,6 @@
 require 'active_record'
 
 ActiveRecord::Base.establish_connection(
-  :adapter => 'sqlite3',
-  :database =>  'rsvp.sqlite3.db'
+  :adapter => 'postgresql',
+  :database =>  'rsvp'
 )
